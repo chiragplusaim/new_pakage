4f023c19e85aa0d32b248b7d0db50a77a9948ac8
chiragplusaim <chirag.ghevariya@plusaim.com>
2017-12-27 11:52:09 +0530

    composer file changes

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,6 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'evalue\\crud\\' => array($baseDir . '/evalue/crud/src'),
+    'evalue\\crud\\' => array($baseDir . '/src'),
     'App\\' => array($baseDir . '/app'),
 );
